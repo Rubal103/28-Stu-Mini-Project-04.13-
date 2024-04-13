8a8d474aac366fbb2e797afa4d8b6193cfe83fb3
Rubal <rubal103@gmail.com>
2024-04-13 15:59:25 -0400

    new repo

---
+++ public/js/profile.js
@@ -0,0 +1,59 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#project-name').value.trim();
+  const needed_funding = document.querySelector('#project-funding').value.trim();
+  const description = document.querySelector('#project-desc').value.trim();
+
+  if (name && needed_funding && description) {
+    const response = await fetch(`/api/projects`, {
+      method: 'POST',
+      body: JSON.stringify({ name, needed_funding, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create project');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/projects/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+
+document
+  .querySelector('.new-project-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.project-list')
+  .addEventListener('click', delButtonHandler);
+
+
+function doupload() {
+// let data = document.getElementById("file").files[0];
+// let entry = document.getElementById("file").files[0];
+// console.log('doupload',entry,data)
+    
+// fetch('uploads/' + encodeURIComponent(entry.name), {method:'PUT',body:data});
+alert('your file has been uploaded');
+};
+
+
